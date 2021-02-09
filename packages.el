9e258462ebd31e70b986f0725349b22068796a77
Michael Lan <michaellan202@gmail.com>
2021-02-09 15:41:43 -0800

    some more good stuff

--- packages.el
+++ packages.el
@@ -4,3 +4,7 @@
 (package! vimrc-mode)
 
 (package! lsp-julia :recipe (:host github :repo "non-jedi/lsp-julia"))
+
+(package! emacs-everywhere :recipe (:host github :repo "tecosaur/emacs-everywhere"))
+
+(unpin! doom-themes)
