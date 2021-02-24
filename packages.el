05c55f50419c4b00c15e3300ac6618e87a6254e5
Michael Lan <michaellan202@gmail.com>
2021-02-24 12:33:18 -0800

    cool acme themes

--- packages.el
+++ packages.el
@@ -3,6 +3,9 @@
 
 (package! vimrc-mode)
 
+(package! nofrils-acme-theme)
+(package! acme-theme)
+
 (package! lsp-julia :recipe (:host github :repo "non-jedi/lsp-julia"))
 
 ;; (package! emacs-everywhere :recipe (:host github :repo "tecosaur/emacs-everywhere"))
