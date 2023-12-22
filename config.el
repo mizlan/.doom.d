babda5c0af2946d03626b358511c763060efbc13
Michael Lan <michaellan202@gmail.com>
2023-12-21 19:31:50 -0800

    Anew

--- config.el
+++ config.el
@@ -1,84 +1,79 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Roboto Mono")
-      doom-variable-pitch-font (font-spec :family "Helvetica Neue"))
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-(delq! 'special-mode global-hl-line-modes)
 
-(setq doom-theme 'modus-operandi)
-(setq display-line-numbers-type nil)
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-(setq user-full-name "Michael Lan"
-      user-mail-address "michaellan202@gmail.com")
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "JetBrains Mono" :size 20))
 
-(map! :leader "SPC" #'counsel-M-x )
-(map! "C-/" #'imenu)
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
 
-(setq lsp-enable-folding t)
-(setq lsp-folding-range-limit 100)
-(setq lsp-clients-clangd-args
-      '("-j=3"
-        "--background-index"
-        "--clang-tidy"
-        "--completion-style=detailed"
-        "--header-insertion=iwyu"))
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
-(after! lsp-clangd (set-lsp-priority! 'clangd 2))
-
-(setq lsp-julia-default-environment "~/.julia/environments/v1.5")
-
-(setq-hook! 'clojure-mode-hook
-  company-idle-delay 0.1
-  company-minimum-prefix-length 1)
-
-(custom-set-faces!
-  '(mode-line :family "Roboto Mono" :height 0.8)
-  '(mode-line-inactive :family "Roboto Mono" :height 0.8))
-
-(setq doom-modeline-height 25)
-
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-(setq org-superstar-headline-bullets-list '("❖" "◉" "◈" "○" "⁘"))
-(setq org-ellipsis " ▼")
-(setq org-hide-emphasis-markers t)
 
-(setq evil-snipe-scope 'visible)
 
-(add-hook! 'elfeed-search-mode-hook 'elfeed-update)
-(after! elfeed
-  (setq elfeed-search-filter "@1-month-ago"))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
 
-(setq doom-themes-treemacs-enable-variable-pitch nil)
-
-(use-package! keycast
-  :commands keycast-mode
-  :config
-  (define-minor-mode keycast-mode
-    "Show current command and its key binding in the mode line."
-    :global t
-    (if keycast-mode
-        (progn
-          (add-hook 'pre-command-hook 'keycast--update t)
-          (add-to-list 'global-mode-string '("" mode-line-keycast " ")))
-      (remove-hook 'pre-command-hook 'keycast--update)
-      (setq global-mode-string (remove '("" mode-line-keycast " ") global-mode-string))))
-  (custom-set-faces!
-    '(keycast-command :inherit doom-modeline-debug
-                      :height 0.9)
-    '(keycast-key :inherit custom-modified
-                  :height 1.1
-                  :weight bold)))
-
-(blink-cursor-mode)
-(fringe-mode 8)
-
-(setq modus-themes-completions 'opinionated)
-(setq modus-themes-mode-line nil)
-
-(after! doom-modeline
-  (setq doom-modeline-enable-word-count nil
-        doom-modeline-buffer-encoding nil
-        doom-modeline-buffer-file-name-style 'file-name
-        line-number-mode nil
-        column-number-mode nil
-        size-indication-mode nil))
+(setq which-key-idle-delay 0.5)
